adbb1f12eccdb9861109c76f8688f0d8c0957f16
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-05-31 19:21:01 -0700

    initialized virtual environment

--- bio-strong/RNA.py
+++ bio-strong/RNA.py
@@ -1,4 +1,11 @@
 def transcribe(str):
+    """
+    Returns RNA transcription of a given DNA string STR.
+
+    >>> transcribe('ATGC')
+    AUGC
+
+    """
     rna = ''
     for c in str:
         if c == 'T':
