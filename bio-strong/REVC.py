59fbba6345b6b858e49123d757f9576ba8a535f8
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-06-14 14:54:32 -0700

    minor change to revc function

--- bio-strong/REVC.py
+++ bio-strong/REVC.py
@@ -6,12 +6,12 @@ def revComp(str):
     rev = reverse(str)
     revComp = ''
     for c in rev:
-        if c == 'A':
+        if c == 'A' or c == "a":
             revComp = revComp + 'T'
-        if c == 'T':
+        if c == 'T' or c == "t":
             revComp = revComp + 'A'
-        if c == 'G':
+        if c == 'G' or c == "g":
             revComp = revComp + 'C'
-        if c == 'C':
+        if c == 'C' or c == "c":
             revComp = revComp + 'G'
     return revComp
