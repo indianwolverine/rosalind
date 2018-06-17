443dc30aaa371e5c682b69856ae50ffe6cf46073
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-06-16 22:02:19 -0700

    all bugs fixed with CONS.py

--- bio-strong/CONS.py
+++ bio-strong/CONS.py
@@ -8,7 +8,18 @@ else:
     with open(sys.argv[1], 'r') as f:
         contents = f.readlines()
     A, T, G, C, new_contents = [], [], [], [], []
-    [new_contents.append(contents[i]) for i in range(1, len(contents), 2)]
+
+    i = 0
+    while i < len(contents):
+        if contents[i][0] == ">":
+            j = i + 1
+            dna_string = ""
+            while j < len(contents) and contents[j][0] != ">":
+                dna_string += contents[j].strip("\n")
+                j += 1
+            new_contents.append(dna_string)
+        i += 1
+
     for i in range(len(new_contents[0])):
         A.append(0)
         T.append(0)
@@ -25,7 +36,7 @@ else:
             elif cons[index] == "C":
                 C[index] += 1
     consensus = ""
-    for i in range(len(A) - 1):
+    for i in range(len(A)):
         value = max(A[i], T[i], G[i], C[i])
         if value == A[i]:
             consensus += "A"
@@ -47,5 +58,5 @@ else:
         string_C += str(i) + " "
 
     print(consensus)
-    print("A:", string_A[:-2], "\nC:", string_C[:-2],
-          "\nG:", string_G[:-2], "\nT:", string_T[:-2])
+    print("A:", string_A, "\nC:", string_C,
+          "\nG:", string_G, "\nT:", string_T)
